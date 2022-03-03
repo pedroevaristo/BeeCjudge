7e5c5704caa28a7603a1af8862ef546ff6f2304e
pedroevaristo <hattoriminamoto@gmail.com>
2022-03-03 18:02:57 -0300

    3222

--- exercicios/bee1113.c
+++ exercicios/bee1113.c
@@ -4,17 +4,13 @@
 #include<string.h>
 
 int main(){
-int x,y,z,a, v[4][2];
+int x,y;
+scanf("%d %d", &x, &y);
+    if(x > y){
+        printf("Crescente.");
+    }
+    else{
+        printf("Decrescente.");
+    }
 
-    for(x=0;x<4;x++){
-        for(y=0;y<2;y++){
-            scanf("%d %d", &v[x][y]);
-        }
-    }
-    printf("\n-----\n");
-    for(x=0;x<4;x++){
-        for(y=0;y<2;y++){
-            printf("%d %d\n", v[x][y]);
-        }
-    }
 }
